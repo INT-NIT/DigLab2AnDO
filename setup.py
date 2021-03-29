e2c3733c8e62eff842b470c4d0795ad9ef97d3e3
slowblitz <jeremygarcia937@gmail.com>
2021-03-29 11:15:27 +0200

    update description in setup.py

--- setup.py
+++ setup.py
@@ -18,8 +18,8 @@ setup(
     author="Julia Sprenger, Jeremy Garcia",
     description="diglab2ando is a tool that allows automatically creating a directory where data and metadata from a "
                 "neuroscientific experiment are stored, and that follows the AnDO (Animal Data Organization) "
-                "specifications ( https://int-nit.github.io/AnDO/ ), using as input a filled pdf "
-                "form generated using the DigLaB tool used at INT",
+                "specifications ( https://int-nit.github.io/AnDO/ ), using as input a filled pdf form or a redcap "
+                "survey/form generated using the DigLaB tool used at INT",
     license='MIT',
     install_requires=[],
     include_package_data=True,
